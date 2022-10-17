365c2be539b2876534fdfa77b7d99b23b2546b95
Arthur Jaspar <a.jaspar@student.fontys.nl>
2022-10-17 10:24:54 +0100

    updated version of functions with comments (button_press function  not working yet)

--- generstor.py
+++ generstor.py
@@ -6,12 +6,22 @@ from fhict_cb_01.CustomPymata4 import CustomPymata4
 from datetime import datetime
 import time
 import csv
+import time, sys
+from pynput.keyboard import Key, Controller #To make this import work, open command promt and put in "pip install pynput".
 
 DHTPIN = 12
-LED1 = 4
-LED2 = 5
+RED_LED = 4
+GREEN_LED = 5
+BUTTON1PIN = 8
+BUZZER = 3
+keyboard = Controller() #Function to make keys simulated in the code.
 
 def current_time():
+
+    """arg: Importing datetime so you can see the current time the from the device that is running the code,
+        we also make the time look nice, with the am/pm as well and removing unnecessary.
+        return: the time in an nice formatted way."""
+
     rightNow = datetime.now()
     time = rightNow.strftime("%I:%M:%S%p")
     time = time.lstrip('0')
@@ -19,21 +29,61 @@ def current_time():
     return time
 
 def current_temp():
+
+    """arg: Uses the arduino temperature sensor to get data.
+    return: The temperature."""
+
     global temperature
     humidity, temperature, timestamp = board.dht_read(DHTPIN)
     return temperature
 
 def setup():
+
+    """arg: Connects the arduino with the code you're running in visual studio code, 
+    as well as the equipment used on the rich shield on top of the Arduino.
+    return: Arduino connection and rich shield."""
+
     global board
     board = CustomPymata4(com_port = "COM6")
+    board.set_pin_mode_digital_input_pullup(BUTTON1PIN)
     board.set_pin_mode_dht(DHTPIN, sensor_type=11, differential=.05)
 
 def countdown(t):
+
+    """arg: Countdown that you can set to a fixed time, formatted into minutes and seconds.
+    return: a timer with mintues and seconds"""
+
     while t:
         mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
+        timer = '{:02d}.{:02d}'.format(mins, secs)
         print(timer, end="\r")
         time.sleep(1)
         t -= 1
+        board.displayShow(timer)
+        board.digital_write(RED_LED, 1)
 
-countdown(900)
+def button_press():
+
+    """arg: button press on the rich shield converted to the keypresses Ctrl + c
+    return: button 1 on the rich shield is now Ctrl + c"""
+
+    level, time_stamp = board.digital_read(BUTTON1PIN)
+    if level == 1:
+        keyboard.press(Key.ctrl)
+        keyboard.press('c')
+        time.sleep(0.1)
+        keyboard.release('c')
+        keyboard.release(Key.ctrl)
+
+setup()
+button_press()
+while True:
+    try:
+        countdown(900)
+    except KeyboardInterrupt: #crtl+C
+        tone(3 )
+        board.digital_write(RED_LED, 0)
+        board.digital_write(GREEN_LED, 1)
+        print ('shutdown')
+        board.shutdown()
+        sys.exit(0)
