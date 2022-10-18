1baf1f667d71f371820816c421c72a704d27e74f
Thomas Vasanth <t.vasanththomas@student.fontys.nl>
2022-10-18 13:22:56 +0200

    sa

--- app.py
+++ app.py
@@ -2,31 +2,50 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+
+data = []
+# oven data
+
+
+
+login_base={
+    1 : {'username':'mario', 'password': 'italy'},
+    2: {'username' : 'luigi' , 'password': 'italianpizza'}
+}
+# login for primary stakeholders
+
+
+
+
 @app.route('/')
 def menu_page():
+    #The main menu
      return render_template('index.html')
-
-
-
-@app.route('/sign_up')
-def sign_up_page():
-    return render_template('signup.html')
 
 
 
 @app.route('/login')
 def login_page():
+    # the login page for admins to log into admin page
     return render_template('login.html')
+
+
+@app.route('/order')
+def order_page():
+    # The page where customers and mario can place orders
+    return render_template('order.html')
+
 
 
 
 @app.route('/admin')
+# the page for mario and luigi
 def admin_page():
     return render_template('admin.html', ovenData = data)
 
 
 
-data = []
+
 @app.route('/orderUpdate', methods = ['POST'])
 def get_data():
     receivedData = request.get_json()
@@ -43,10 +62,6 @@ def get_data():
 
 
 
-login_base={
-    1 : {'username':'mario', 'password': 'italy'},
-    2: {'username' : 'luigi' , 'password': 'italianpizza'}
-}
 
 check_login =False
 @app.route('/admin_login', methods = ['POST'])
