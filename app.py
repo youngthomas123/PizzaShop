0bd287fa2ee9574efb84f3b0303a58e7dfa5d456
Arthur Jaspar <a.jaspar@student.fontys.nl>
2022-10-21 10:48:10 +0100

    trying to get latest version

--- app.py
+++ app.py
@@ -1,45 +1,42 @@
-
-
 from random import randint
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
 
+data = []
+# oven data
 
 
 
-login_base={
-    1 : {'username':'mario', 'password': 'italy'},
-    2: {'username' : 'luigi' , 'password': 'italianpizza'}
-}
-# login for primary stakeholders
-
-
-
-
-@app.route('/')
-def menu_page():
-    #The main menu
-     return render_template('index.html')
-
-
-
-@app.route('/login')
-def login_page():
-    # the login page for admins to log into admin page
-    return render_template('login.html')
+app.route('/wrong')
+def wrong_username():
+    return render_template('wrong.html')
 
 
 @app.route('/order')
 def order_page():
     # The page where customers and mario can place orders
     return render_template('order.html',orderlist=orderList)
+    
+    return render_template('order.html',orderlist=orderlist)
+
+
 
 with open('generations.csv', 'w', newline='') as o:
     writer = csv.writer(o)
 
 orderList = []
+@app.route('/wrong_login')
+def wrong_logon_page():
+    return render_template('wrong.html')
+
+
+
+
+order = []
+orderlist = []
 @app.route('/ordering', methods = ['POST'])
 def add_order():
     global orderList
@@ -58,15 +55,29 @@ def add_order():
     if lentghOrder > 15:
         orderList.pop(0)
 
-    return redirect('/order')
+    global order, orderlist
+      
+    received_data = request.form["pizza"]
+    if (received_data !="complete") : 
+        order.append(received_data)
+        print (order)
+    else : 
+        unique= str(randint(100, 999))
+        order.insert(0,{"order_id" : unique})
+        orderlist.append(order)
+        order = []
+        print (order)
+        print(orderlist)
 
+    print (orderlist)
+    
+    return redirect('/order')
 
 
 @app.route('/admin')
 # the page for mario and luigi
 def admin_page():
-
-    return render_template('admin.html', ovenData = data,orderlist=orderList)
+    return render_template('admin.html', oven_data=oven_data,orderlist=orderlist)
 
 
 oven_data = {
@@ -81,6 +92,18 @@ oven_data = {
 def get_data():
     global oven_data
     receivedData = request.get_json()
+    newData = (receivedData['ovenResponse'],
+    receivedData['countdown'],
+    receivedData['time'],
+    receivedData['temp'])
+    print (receivedData)
+    print(newData)
+    print(data)
+
+    if len(data) >= 1:
+        data.pop(0)
+        
+    data.append(newData)
     oven_data = {
     "ovenResponse" : receivedData["ovenResponse"],
     "countdown"    : receivedData["countdown"],
@@ -93,24 +116,12 @@ def get_data():
     return redirect ('/admin')
 
 
-
-
-check_login =False
-@app.route('/admin_login', methods = ['POST'])
-def admin_login():
-    check_login =False
-    
-    login = request.form
-    name =login ['sent_username']
-    password = login['sent_password']
-    for _ in login_base :
-        if(name== login_base[_]['username'] and password == login_base[_]['password']) :
-            check_login= True
-            break
     if (check_login==True):
-        return redirect('/')
-    else :
         return redirect('/admin')
+    else :
+        return redirect('/wrong')
+        return redirect('/wrong_login')
+
     
        
 
